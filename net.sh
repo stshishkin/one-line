4e2134b87a28429c2f103e055a6f8fb093de524f
Stepan Shishkin <st.shishkin@gmail.com>
2017-02-15 00:21:05 +0300

    Update net.sh
    added command to get wan address

--- net.sh
+++ net.sh
@@ -1,2 +1,6 @@
 #return ip of default route interface
 ip addr show dev $(ip route get 8.8.8.8 | grep -Po '(?<=dev )[^ ]+') | grep -Po '(?<=inet )[^/ ]+'
+#return public ip if available
+if ping -c 1 ns2.google.com &> /dev/null ; then \
+echo $(dig TXT +short o-o.myaddr.l.google.com @ns2.google.com | sed "s/\"//g" ); \
+else echo offline; fi

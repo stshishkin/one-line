0955508fcee27756d9202a879da7d1ca6b415873
Stepan Shishkin <st.shishkin@gmail.com>
2017-03-15 12:43:38 +0300

    add online function

--- net.sh
+++ net.sh
@@ -4,3 +4,19 @@ ip addr show dev $(ip route get 8.8.8.8 | grep -Po '(?<=dev )[^ ]+') | grep -Po 
 if ping -c 1 ns2.google.com &> /dev/null ; then \
 echo $(dig TXT +short o-o.myaddr.l.google.com @ns2.google.com | sed "s/\"//g" ); \
 else echo offline; fi
+#test connect to specified host and exit when it gets online 
+function online (){
+     if [[ -z $1 ]]; then
+         test_host=8.8.8.8
+     else
+         test_host=$1
+     fi
+     while true
+          do
+              ping -c 1 $test_host &> /dev/null 2>&1
+              if [[ $? -eq 0 ]]; then
+                  echo online;
+                  break
+              fi
+          done
+}

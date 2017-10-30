962fb1ae7f093d857af489c5ed46cb73bc8ddc9f
stshishkin@users.noreply.github.com <st.shishkin@gmail.com>
2017-10-30 17:15:06 +0300

    add fast export from mysql

--- db.sh
+++ db.sh
@@ -5,3 +5,7 @@ mysql -uroot -prootpassword -e'SELECT table_schema "database_name", sum( data_le
 # it's not a secret, that size of database not equal size of dump file.
 # how to calculate real size of SQL-dump without actually write on disk and check? (shows size in bytes)
 mysqldump -uroot -prootpassword database_name | wc -c
+
+# how to export data from mysql table in csv by one command?
+# here is an answer)
+mysql -uuser -puserpass database -e"select * from some_table where some_field like '%some_value%' into outfile '/tmp/some_file.csv' fields terminated by ';' lines terminated by '\n';"
